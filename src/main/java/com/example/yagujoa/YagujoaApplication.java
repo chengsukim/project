fac97d0596a0761848bc991a1f04241f56d03254
깃chengsukim <kcs981123@naver.com>
2024-01-02 11:04:54 +0900

    https://github.com/chengsukim/project.git

---
+++ src/main/java/com/example/yagujoa/YagujoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.yagujoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagujoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagujoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yagujoa/YagujoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yagujoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagujoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
